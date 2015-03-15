5b110a17c7894e1ee82e0d90ca609cd6d3ff4aad
Ondrej Mirtes <ondrej@mirtes.cz>
2015-03-15 15:45:33 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Papertrail – Google Chrome extension for clickable links
+=========================
+
+This Google Chrome extension creates clickable links in log messages.
+
+If a log message contains `<link>` (e. g. `<https://www.google.com/>`), this URL is turned into a clickable link.
+
+It is [available in the Google Chrome Web Store](https://chrome.google.com/webstore/detail/papertrail-clickable-link/dbgmfbodfabdpflmlmipapijbmgcgjfg).
---
+++ clickable.js
@@ -0,0 +1,36 @@
+var replaceTextWithLinks = function (element) {
+	if (element.classList.contains('linkified')) {
+		return;
+	}
+
+	var text = element.textContent;
+	var textWithLinks = text.replace(/<([^<>]*)>/g, '&lt;<a href="$1">$1</a>&gt;');
+	element.innerHTML = textWithLinks;
+	element.classList.add('linkified');
+};
+
+var observer = new MutationObserver(function(mutations) {
+	mutations.forEach(function(mutation) {
+		for (var i = 0; i < mutation.addedNodes.length; i++) {
+			var element = mutation.addedNodes[i];
+			if (!('querySelectorAll' in element)) {
+				// some added elements do not have this method
+				continue;
+			}
+
+			var messages = element.querySelectorAll('span.message');
+			for (var j = 0; j < messages.length; j++) {
+				replaceTextWithLinks(messages[j]);
+			}
+		}
+	});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+	var messages = document.querySelectorAll('span.message');
+	for (var i = 0; i < messages.length; i++) {
+		replaceTextWithLinks(messages[i]);
+	}
+
+	observer.observe(document, { childList: true, subtree: true });
+});
